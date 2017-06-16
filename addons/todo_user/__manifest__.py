764817dca32dd67bb880ba51957e0a97e0b99233
spacegoing <spacegoing@gmail.com>
2017-06-16 16:58:55 +0800

    [New] new module

---
+++ addons/todo_user/__manifest__.py
@@ -0,0 +1,6 @@
+{
+    'name':'Multiuser To-Do',
+    'description': 'Extend the To-Do app to multiuser.',
+    'depends': ['todo_app'],
+    'author': 'hdwolf',
+}
--- addons/todo_user/todo_model.py
+++ addons/todo_user/todo_model.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from odoo import models, fields, api, netsvc
+from odoo import models, fields, api
 
 
 class TodoTask(models.Model):
@@ -19,12 +19,10 @@ class TodoTask(models.Model):
             task.is_done = not task.is_done
         return True
 
-    @api.model
+    @api.multi
     def do_clear_done(self):
-        dones = self.search(['is_done', '=', True])
-        print dones
-        import ipdb; ipdb.set_trace()
-        dones.write({'active':False})
+        # dones = self.search(['is_done', '=', True])
+        # dones.write({'active':False})
         return True
 
 
